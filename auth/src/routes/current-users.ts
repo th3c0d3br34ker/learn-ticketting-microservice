23e0390e7eefbdf01721c2bd841ecb51a1e53c1b
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2021-06-28 18:57:25 +0530

    🔧 updated signin, signup routes

--- auth/src/routes/current-users.ts
+++ auth/src/routes/current-users.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express';
+import { verify } from 'jsonwebtoken';
 
 const router = Router();
 
 router.get('/api/users/current-user', (req, res) => {
-  res.send('Hi there!');
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = verify(req.session.jwt, process.env.JWT_KEY!);
+    return res.send({ currentUser: payload });
+  } catch (error) {
+    console.log('Error: Invalid JWT Token!');
+    return res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,59 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import { body } from 'express-validator';
+import { sign } from 'jsonwebtoken';
+import { BadRequestError } from '../errors/bad-request-error';
+
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { Password } from '../services/password';
 
 const router = Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hi there! Signin');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid!'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('Password must be supplied!'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      console.log(`Email : ${email} not found!`);
+      throw new BadRequestError('Invalid credentials!');
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordsMatch) {
+      console.log(`Email : ${email} incorrect passowrd!`);
+      throw new BadRequestError('Invalid credentials!');
+    }
+
+    const userJWT = sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    req.session = { jwt: userJWT };
+
+    return res.send({
+      success: true,
+      message: `Signin successful with email : ${email}`,
+      data: { existingUser },
+    });
+  }
+);
 
 export { router as signInRouter };
